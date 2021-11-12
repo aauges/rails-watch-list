41747b8084c81608556822ad708690f461a6c5fe
albert auge <aaugeserrano@gmail.com>
2021-11-12 16:23:41 +0100

    push with some style

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,5 @@
 class BookmarksController < ApplicationController
-  before_action :set_bookmark, only: :destroy
-  before_action :set_list, only: [:new, :create]
+  before_action :find_list, only: [ :new, :create ]
 
   def new
     @bookmark = Bookmark.new
@@ -12,26 +11,23 @@ class BookmarksController < ApplicationController
     if @bookmark.save
       redirect_to list_path(@list)
     else
-      render :new
+      render template: "lists/show"
     end
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(@bookmark.list)
+    redirect_to root_path
   end
 
   private
 
+  def find_list
+    @list = List.find(params[:list_id])
+  end
+
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
-  end
-
-  def set_bookmark
-    @bookmark = Bookmark.find(params[:id])
-  end
-
-  def set_list
-    @list = List.find(params[:list_id])
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,12 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
     @bookmark = Bookmark.new
-    @review = Review.new(list: @list)
   end
 
   def new
@@ -17,25 +16,21 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to list_path(@list)
+    redirect_to list_path(@list)
     else
       render :new
     end
   end
 
   def destroy
+    @list = List.find(params[:id])
     @list.destroy
-    redirect_to lists_path
+    redirect_to root_path
   end
-
 
   private
 
-  def set_list
-    @list = List.find(params[:id])
-  end
-
   def list_params
-    params.require(:list).permit(:name, :photo)
+    params.require(:list).permit(:name, :imageUrl)
   end
 end
